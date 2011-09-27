3f7fa8ebb7399183d30c59e32a15cab545c6b405
Fred Nias <fred.nias@gmail.com>
2011-09-27 21:43:19 +0200

    refactoring

--- wow-gold.lua
+++ wow-gold.lua
@@ -1,7 +1,14 @@
-local frame = CreateFrame("Frame");
-frame:RegisterEvent("PLAYER_ENTERING_WORLD");
-local function eventHandler(self, event, ...)
- print("Hello World! Hello " .. event);
+function WowGold_OnLoad(self)
+ self:RegisterEvent("PLAYER_ENTERING_WORLD");
+ self:RegisterEvent("DUEL_REQUESTED");
+ self:SetScript("OnEvent", eventHandler);
 end
-frame:SetScript("OnEvent", eventHandler);
+
+function eventHandler(self, event, ... )
+	if event == "PLAYER_ENTERING_WORLD"
+	then
+		print("Hello World! Hello " .. event);
+
+	end
+end
 
