5292621078ea01d480aa062ad6e561283bc0af54
Fred Nias <fred.nias@gmail.com>
2011-10-02 16:23:45 +0200

    affiche le nombre de gold

--- wow-gold.lua
+++ wow-gold.lua
@@ -1,5 +1,7 @@
 function WowGold_OnLoad(self)
+ self:RegisterEvent("PLAYER_ENTERING_WORLD");
  self:RegisterEvent("DUEL_REQUESTED");
+ self:RegisterEvent("PLAYER_MONEY");
  self:SetScript("OnEvent", eventHandler);
 end
 
@@ -7,6 +9,13 @@ function eventHandler(self, event, ... )
 	if event == "DUEL_REQUESTED"
 	then
 		CancelDuel();
+	end
+
+	if (event == "PLAYER_ENTERING_WORLD") or (event == "PLAYER_MONEY")
+	then
+		MiniWindowText:Show();
+		WowGold:Show();
+		MiniWindowText:SetText(GetCoinText(GetMoney()));
 	end
 end
 
